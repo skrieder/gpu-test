5c727554ebed1eec270bc6ee489d4839f1f8e742
Scott J. Krieder <skrieder@iit.edu>
2012-06-14 09:05:18 -0500

    modified bench to run 100, 200, 400, 800 sleep seconds

--- benchmark.sh
+++ benchmark.sh
@@ -3,12 +3,12 @@ throttle=1
 nkernels=1
 # 1 through 5 for the throttles
 # for i in {1..5}
-for i in {1..1}
+for i in {1..5}
 do
 sleeptime=100
     # 1 through 8 for the sleeptimes
     # for k in {1..8}
-    for k in {1..1}
+    for k in {1..4}
     do
 	echo ""
 	echo ""
@@ -18,7 +18,7 @@ sleeptime=100
 	echo "The number of kernels: " $nkernels
 	
 	# run swift
-	time swift -tc.file tc.data -sites.file sites/$throttle-throttle-sites.xml gpu.swift -nkernels=$nkernels -kernel_time=$sleeptime 2> log.txt
+	time swift -tc.file tc.data -sites.file sites/$throttle-throttle-sites.xml gpu.swift -nkernels=$nkernels -kernel_time=$sleeptime
 	
         # double the sleeptime
 	sleeptime=$(expr $sleeptime + $sleeptime)

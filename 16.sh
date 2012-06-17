88421c2b6f586c4e1ea3b3f96b80821beac2a0f0
Scott J. Krieder <skrieder@iit.edu>
2012-06-17 14:24:19 -0500

    ran tests, moved into logs/

---
+++ 16.sh
@@ -0,0 +1,26 @@
+#! /bin/bash                                                                                                                                                                       
+throttle=1
+nkernels=16
+# 1 through 5 for the throttles
+# for i in {1..5}
+for i in {1..1}
+do
+sleeptime=100
+    # 1 through 8 for the sleeptimes
+    # for k in {1..8}
+    for k in {1..4}
+    do
+	echo ""
+	echo ""
+	echo ""
+	echo "Throttle count equals: " $throttle    
+	echo "Sleeptime is equal to: " $sleeptime
+	echo "The number of kernels: " $nkernels
+	
+	# run swift
+	time swift -tc.file tc.data -sites.file sites/$throttle-throttle-sites.xml gpu.swift -nkernels=$nkernels -kernel_time=$sleeptime
+	
+        # double the sleeptime
+	sleeptime=$(expr $sleeptime + $sleeptime)
+    done
+done
